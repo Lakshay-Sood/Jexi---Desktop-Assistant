fe91278625ba57eb169a6061a110b1ef1f150157
Lakshay Sood <lksood@ymail.com>
2021-02-22 11:41:31 +0530

    Basic functionality working (in test.py)

--- automation.py
+++ automation.py
@@ -2,21 +2,75 @@ import pyautogui as pag
 import speech_recognition as sr
 import pyttsx3 as tts
 
+# Initialize the recognizer
+r = sr.Recognizer()
+r2 = sr.Recognizer()
 
-def goToSite(website):
-    pag.hotkey('alt', 'tab')
-    pag.hotkey('ctrl', 't')
-    pag.hotkey('ctrl', 'l')
-    pag.write(website, interval=0.1)
-    pag.press('enter')
+# Function to convert text to
+# speech
 
 
-def openFileExplorer():
-    pag.hotkey('win', 'e')
+def SpeakText(command):
+
+    # Initialize the engine
+    engine = tts.init()
+    engine.say(command)
+    engine.runAndWait()
+
+
+# Loop infinitely for user to
+# speak
+
+print('listening...\n')
+while(1):
+
+    # Exception handling to handle
+    # exceptions at the runtime
+    try:
+
+        # use the microphone as source for input.
+        with sr.Microphone() as source2:
+
+            # wait for a second to let the recognizer
+            # adjust the energy threshold based on
+            # the surrounding noise level
+            r.adjust_for_ambient_noise(source2, duration=0.5)
+
+            # listens for the user's input
+            audio2 = r.listen(source2, )
+
+            # Using ggogle to recognize audio
+            # MyText = r.recognize_google(audio2)
+            MyText2 = r2.recognize_google(audio2)
+
+            # MyText = MyText.lower()
+
+            # print("Did you say "+MyText)
+            print("Did you say "+MyText2)
+            # SpeakText(MyText)
+
+    except sr.RequestError as e:
+        print("Could not request results; {0}".format(e))
+
+    except sr.UnknownValueError:
+        print("unknown error occured")
+
+#############################################
+
+# def goToSite(website):
+#     pag.hotkey('alt', 'tab')
+#     pag.hotkey('ctrl', 't')
+#     pag.hotkey('ctrl', 'l')
+#     pag.write(website, interval=0.1)
+#     pag.press('enter')
+
+
+# def openFileExplorer():
+#     pag.hotkey('win', 'e')
 
 
 # website = input("Enter website name: ")
 # goToSite(website)
 
 # print(pag.KEY_NAMES)
-pag.hotkey('fn', 'f9')
+# pag.hotkey('fn', 'f9')
---
+++ test.py
@@ -0,0 +1,84 @@
+# Python 2.x program for Speech Recognition
+import speech_recognition as sr
+import pyautogui as pag
+
+
+def doAutomatedTask(text):
+    if text == 'increase volume':
+        for i in range(10):
+            pag.press('volumeup')
+
+    if text == 'decrease volume':
+        for i in range(10):
+            pag.press('volumedown')
+
+    if text == 'music stop' or text == 'music play':
+        pag.press('playpause')
+
+    if text == 'next song':
+        pag.press('nexttrack')
+
+    elif text == 'switch application':
+        pag.hotkey('alt', 'tab')
+
+    elif text == 'open new tab':
+        pag.hotkey('ctrl', 't')
+
+    elif text == 'go to facebook.com':
+        pag.hotkey('ctrl', 'l')
+        pag.write('facebook.com')
+        pag.press('enter')
+
+
+# enter the name of usb microphone that you found
+# using lsusb
+# the following name is only used as an example
+# mic_name = "USB Device 0x46d:0x825: Audio (hw:1, 0)"
+
+# Sample rate is how often values are recorded
+sample_rate = 48000
+
+# Chunk is like a buffer. It stores 1024 samples (bytes of data)
+# here.
+# (it is advisable to use powers of 2 such as 1024 or 2048)
+chunk_size = 1024
+# Initialize the recognizer
+r = sr.Recognizer()
+
+# generate a list of all audio cards/microphones
+# mic_list = sr.Microphone.list_microphone_names()
+
+# the following loop aims to set the device ID of the mic that
+# we specifically want to use to avoid ambiguity.
+# for i, microphone_name in enumerate(mic_list):
+#     if microphone_name == mic_name:
+#         device_id = i
+
+# use the microphone as source for input. Here, we also specify
+# which device ID to specifically look for incase the microphone
+# is not working, an error will pop up saying "device_id undefined"
+while(1):
+
+    with sr.Microphone(sample_rate=sample_rate,
+                       chunk_size=chunk_size) as source:
+        # wait for a second to let the recognizer adjust the
+        # energy threshold based on the surrounding noise level
+        r.adjust_for_ambient_noise(source)
+        print("Say Something")
+        # listens for the user's input
+        audio = r.listen(source)
+        print('------')
+        try:
+            text = r.recognize_google(audio)
+            print('======')
+            print("you said: " + text)
+            # doAutomatedTask(text)
+
+        # error occurs when google could not understand what was said
+
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+
+        except sr.RequestError as e:
+            print(
+                "Could not request results from Google Speech Recognition service {0}".format(e))
